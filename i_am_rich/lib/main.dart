c43039f8695abffc08b91df9f7b83c74fb04055e
Carolina Bonturi <carolina.bonturi@nubank.com.br>
2020-06-21 15:01:16 -0300

    Add background color and center the image

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -3,8 +3,17 @@ import 'package:flutter/material.dart';
 // The main function is the starting point for all our Flutter apps
 void main() => runApp(
       MaterialApp(
-        home: Center(
-          child: Text('Hello, world!'),
+          home: Scaffold(
+              body: Center(
+                child: Image(
+                  image: NetworkImage( 'https://assets.entrepreneur.com/content/3x2/2000/20160305000536-diamond.jpeg'),
+                  alignment: Alignment.topCenter,
+                ),
+              ),
+        appBar: AppBar(
+          title: Text('I am rich'),
+          backgroundColor: Colors.blueGrey[900],
         ),
-      ),
+        backgroundColor: Colors.blueGrey,
+      )),
     );
